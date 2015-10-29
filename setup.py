5cb6fee738265db32f38a302284b38000feb70b8
ishu3101 <ishu3101@hotmail.com>
2015-10-29 14:20:41 +1300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='shortn',
+      version='0.0.1',
+      description='Shorten url using bit.ly, j.mp, t.cn, is.gd, v.gd, tiny.cc.',
+      author='ishu3101',
+      license='MIT',
+      keywords = "shorten url_shortener shortener short",
+      url='http://github.com/ishu3101/shortn',
+      py_modules=['shortn'],
+      entry_points = {
+        'console_scripts': [
+            'shortn = shortn'
+        ],
+      }
+)
